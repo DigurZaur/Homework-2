72bc05c6b253708efc42c1754edfddcc3d40c674
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 23:54:21 +0300

    Complete Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 4: Напишите программу, которая на вход принимает 
+// натуральное число N, а на выходе показывает его цифры через запятую.
+
+System.Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 10)
+{
+    System.Console.WriteLine(num);
+}
+else if (num == 0)
+{
+    System.Console.WriteLine("0 не является натуральным числом!");
+}
+else
+{
+    while (num > 0)
+    {
+        int part = num % 10;
+        if (num > 9)
+        {
+            System.Console.Write($"{part}, ");
+        }
+        else
+        {
+            System.Console.Write($"{part}.");
+        }
+        num = num / 10;
+    }
+}
