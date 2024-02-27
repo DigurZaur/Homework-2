6420dae496f5a04c8fba57c2d96befe93ff3033f
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 23:32:00 +0300

    Complete Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход 
+// целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+System.Console.WriteLine("Введите число от 10 до 99:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 10 || num > 99)
+{
+    System.Console.WriteLine("Данное число не входит в отрезок [10, 99]!");
+}
+else 
+{
+    int x1 = num / 10;
+    int x2 = num % 10;
+    if (x1 > x2)
+    {
+        System.Console.WriteLine($"Наибольшая цифра числа: {x1}");
+    }
+    else
+    {
+        System.Console.WriteLine($"Наибольшая цифра числа: {x2}");
+    }
+}
