84cfbe141222b71c6865e66db69f0506c392033d
Zaurbek Digurov <digurzaur@yandex.ru>
2024-02-27 23:18:16 +0300

    Complete Task1

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 1: Напишите программу, которая принимает на 
+// вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+System.Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0)
+{
+    System.Console.WriteLine($"Число {num} кратно одновременно 7 и 23");
+}
+else
+{
+    System.Console.WriteLine($"Число {num} НЕ кратно одновременно 7 и 23");
+}
